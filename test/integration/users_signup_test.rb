e00fddfa6cbb59ed6b972e4488c56c5a90949021
バントウフウマ <coolishpoo3@gmail.com>
2020-09-18 06:32:45 +0900

    add test for error_message

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -7,6 +7,9 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       post users_path,params: {user: {name: "",email: "user@invalid",password: "foo",password_confirmation: "bar"}}
     end
     assert_template 'users/new'
+    assert_select "div#error_explanation"
+    assert_select "div.alert"
+    assert_select 'div.alert-danger'      
   end
 
 end
