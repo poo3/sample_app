b2c693cb2c6e681044cd017f45d71beb185212d8
バントウフウマ <coolishpoo3@gmail.com>
2020-09-26 08:11:59 +0900

    add method logged_in

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -11,4 +11,9 @@ module SessionsHelper
       @current_user = User.find_by(id: session[:user_id])
     end
   end
+
+  #ユーザーがログインしていればtrue、その他ならばfalseを返します
+  def logged_in?
+    !current_user.nil?
+  end
 end
