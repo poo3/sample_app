5aad393d5e895a09ce052325b1d9a8792148f35e
バントウフウマ <coolishpoo3@gmail.com>
2020-10-10 19:43:54 +0900

    add edit test of integration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,6 +27,8 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     if @user.update(user_params)
       #更新に成功した場合の処理
+      flash[:success] = "Profile updated"
+      redirect_to @user
     else
       render 'edit'
     end
