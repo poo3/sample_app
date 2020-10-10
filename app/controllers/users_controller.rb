2751cd06e20d3da1a547cd5c0c0ed5749d002972
バントウフウマ <coolishpoo3@gmail.com>
2020-10-10 20:17:11 +0900

    add confirmation_login for edit_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user,only: [:edit,:update]
+
   def new
     @user = User.new
   end
@@ -41,4 +43,13 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name,:email,:password,:password_confirmation)
     end
 
+    #beforeアクション
+
+    #ログイン済みユーザか確認
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = 'Please log in...'
+        redirect_to login_url
+      end
+    end
 end
