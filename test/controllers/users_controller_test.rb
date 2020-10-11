c5ee110b0b62c5001147c7af56621eb94f021b5e
バントウフウマ <coolishpoo3@gmail.com>
2020-10-11 10:41:33 +0900

    modification test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -24,19 +24,21 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_not flash.empty?
     assert_redirected_to login_url
   end
-end
 
-test "should redirect edit when logged in as wrong user" do
-  log_in_as(@other_user)
-  get edit_user_path(@user)
-  assert flash.empty?
-  assert_redirected_to root_url
-end
 
-test "should redicect update when logged in as wrong user" do
-  log_in_as(@other_user)
-  pathch user_path(@user),params:{user:{name:@user.name,
-                                        email:@user.email}}
-  assert flash.empty?
-  assert_redirected_to root_url
+  test "should redirect edit when logged in as wrong user" do
+    log_in_as(@other_user)
+    get edit_user_path(@user)
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
+
+  test "should redicect update when logged in as wrong user" do
+    log_in_as(@other_user)
+    pathch user_path(@user),params:{user:{name:@user.name,
+                                          email:@user.email}}
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
+
 end
