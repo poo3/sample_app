7afa47bf319a8c6fc86e28d87e6fb01cd3203369
バントウフウマ <coolishpoo3@gmail.com>
2020-10-17 09:23:12 +0900

    add user_destroy_action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user,only: [:edit,:update,:index]
+  before_action :logged_in_user,only: [:edit,:update,:index,:destroy]
   before_action :correct_user,only: [:edit,:update]
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User Dleted"
+    redirect_to users_url
+  end
 
   def index
     @users = User.paginate(page:(params[:page]))
