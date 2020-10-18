133fbdcc6267faee4b88e15eabf7fbfbe05d8565
バントウフウマ <coolishpoo3@gmail.com>
2020-10-18 15:26:35 +0900

    add call back create_activation_digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
-  before_save {email.downcase!}
+  attr_accessor :remember_token,:remember_token
+  before_save :downcase_email
+  before_create :create_activation_digest
   validates :name,presence: true,length: {maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email,presence: true,length: {maximum: 255},
@@ -37,4 +38,16 @@ class User < ApplicationRecord
   def forget
     update_attribute(:remember_digest,nil)
   end
+
+  private
+
+    def downcase_email
+      self.email = email.downcase
+    end
+    
+    def create_activation_digest
+      #有効化トークンとダイジェストを作成及代入する
+      self.activation_token = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
 end
