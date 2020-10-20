4d2e68cd235b69fbdfae73ba4322d0ca5a16c7b3
バントウフウマ <coolishpoo3@gmail.com>
2020-10-20 22:36:02 +0900

    modification for the error

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -44,7 +44,8 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 
   test 'login with remembering' do
     log_in_as(@user,remember_me: '1')
-    assert_equal cookies[:remember_token],assigns(:user).remember_token
+    user = assigns(:user)
+    assert_equal cookies[:remember_token],user.remember_token
   end
 
   test 'login without rememmbering' do
