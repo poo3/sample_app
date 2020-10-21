3ab8716d317aebf4a6e576a022065e5e6f3fe7cc
バントウフウマ <coolishpoo3@gmail.com>
2020-10-21 23:46:45 +0900

    modification for not_activated_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   end
 
   def index
-    @users = User.paginate(page:(params[:page]))
+    @users = User.where(activated: true).paginate(page: params[:page])
   end
 
   def new
@@ -19,6 +19,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    redirect_to root_url and return unless @user.activated?
   end
 
   def create
