c5d81f4525b3f9f13dcc99f1ff304739d1d85b6e
バントウフウマ <coolishpoo3@gmail.com>
2020-10-23 14:25:22 +0900

    add create action

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,4 +1,22 @@
 class PasswordResetsController < ApplicationController
   def new
   end
+
+  def create
+    @user = User.find_by(email: params[:email])
+    if @user
+      @user.create_reset_deigest
+      @user.send_password_reset_email
+      flash[:info] = "Email sent with password reset instructions"
+      redirect_to root_url
+    else
+      flash.now[:danger] = "Email address not found"
+      render 'new'
+    end
+  end
+
+  def edit
+    
+  end
+
 end
