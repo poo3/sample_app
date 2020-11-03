22ceba9fdf06361961993da453d991384b3b07ac
バントウフウマ <coolishpoo3@gmail.com>
2020-11-03 11:56:39 +0900

    add passive_relationships

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -83,9 +83,10 @@ class UserTest < ActiveSupport::TestCase
   test "should follow and unfollow a user" do
     michael = users(:michael)
     archer = users(:archer)
-    assert_not micahael.follwing?(archer)
+    assert_not michael.following?(archer)
     michael.follow(archer)
     assert michael.following?(archer)
+    assert archer.followers.include?(michael)
     michael.unfollow(archer)
     assert_not michael.following?(archer)
   end
